74fefd36187207b27abcc364b7bdb46031161b44
Kestrel-opswerks <franz.lopez@academy.opswerks.com>
2024-12-05 02:01:52 +0000

    Added Node Exporter Installation Automation

---
+++ test.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+URL="$1"
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+TAR_FILE=$(basename "$URL")
+EXTRACT_DIR="${TAR_FILE%.tar.gz}"
+
+wget -q --show-progress "$URL"
+
+tar -xzvf "$TAR_FILE"
+
+cd "$EXTRACT_DIR"
+
+sudo cp node_exporter /usr/local/bin 
+
+sudo useradd --no-create-home --shell /bin/false node_exporter 
+
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
+
+sudo cat <<EOF | sudo tee /etc/systemd/system/node_exporter.service >/dev/null
+[Unit] 
+Description=Node Exporter 
+Wants=network-online.target 
+After=network-online.target 
+
+[Service] 
+User=node_exporter  
+Group=node_exporter 
+Type=simple 
+ExecStart=/usr/local/bin/node_exporter 
+
+[Install] 
+WantedBy=multi-user.target 
+EOF
+
+
+sudo systemctl daemon-reload 
+
+sudo systemctl enable --now node_exporter 
+
+sudo systemctl status node_exporter 
